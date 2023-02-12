f769497770d178babb3acd22031d7855a3ad5d73
mit1mit1 <mit1mit1@users.noreply.github.com>
2023-02-12 03:49:24 +0000

    deploy: 846998489f02a9a9922e62d1b3ed712f2212101b

---
+++ _app/immutable/chunks/1-0e764a74.js
@@ -0,0 +1,4 @@
+import { default as default2 } from "../components/error.svelte-3c507b52.js";
+export {
+  default2 as component
+};
---
+++ _app/immutable/chunks/2-4d7eabb2.js
@@ -0,0 +1,4 @@
+import { default as default2 } from "../components/pages/_page.svelte-7eaf315a.js";
+export {
+  default2 as component
+};
